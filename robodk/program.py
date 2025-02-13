e1a64cb6bab4bd9091ae2fad642cb03d988ac11d
NoisNette <david.mihic@gmail.com>
2025-02-14 00:49:50 +0100

    changed tool to OnRobot RG6 gripper

--- robodk/program.py
+++ robodk/program.py
@@ -3,7 +3,8 @@ from robodk import robomath  # Robot toolbox
 
 RDK = robolink.Robolink("localhost", port=20500)
 
-itemlist = RDK.ItemList()
-# print(itemlist)
+robot = RDK.Item("UR3e")
+print(robot.Pose())
 
 disks = [RDK.Item(f"disk_{diam}") for diam in [70, 90, 110, 130]]
+print(disks)
