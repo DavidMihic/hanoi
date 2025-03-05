d14248658d511bc32acf4746b33d4455ced1c334
NoisNette <59642044+NoisNette@users.noreply.github.com>
2025-03-05 16:36:34 +0100

    added joint positions of some targets

--- code/program.py
+++ code/program.py
@@ -1,12 +1,57 @@
 import urx
+import urx.urscript
+import urx.ursecmon  # https://github.com/jkur/python-urx
 from onrobot import RG
 from time import sleep
 
+FAST_VEL = 0.75
+SLOW_VEL = 0.3
+
+HOME_TARGET = [
+    3.1737236976623535,
+    -1.0330191415599366,
+    -1.7965435981750488,
+    -1.8829833469786585,
+    1.5768370628356934,
+    0.4197049140930176,
+]
+ABOVE_ROD_1_TARGET = [
+    2.5289292335510254,
+    -1.404278115635254,
+    -2.0054268836975098,
+    -1.3028734487346192,
+    1.5768728256225586,
+    -0.17434579530824834,
+]
+
+
+def openGripper():
+    rg.open_gripper()
+
+    while True:
+        sleep(0.5)
+        if not rg.get_status()[0]:
+            break
+
+
+def closeGripper():
+    rg.close_gripper()
+
+    while True:
+        sleep(0.5)
+        if not rg.get_status()[0]:
+            break
+
 
 def main():
-    rg.open_gripper()
-    sleep(2)
-    rg.close_gripper()
+    rob.set_gravity([0.0, 0.0, 9.82])
+    rob.set_payload(1.2)
+
+    # print(rob.getj())
+
+    rob.movej(HOME_TARGET, vel=FAST_VEL)
+    rob.movej(ABOVE_ROD_1_TARGET, vel=FAST_VEL)
+    rob.movel([0, 0, -0.23, 0, 0, 0], vel=SLOW_VEL, relative=True)
 
 
 if __name__ == "__main__":
