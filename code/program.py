7f691a004af4698739b800d0bc626026d608d155
NoisNette <59642044+NoisNette@users.noreply.github.com>
2025-03-14 16:06:12 +0100

    changed opening and closing gripper functions, refactored some code

--- code/program.py
+++ code/program.py
@@ -8,30 +8,21 @@ import math3d as m3d
 # m/s, m/s^2
 FAST_VEL = 0.75
 SLOW_VEL = 0.3
-ACCEL = 0.01
+ACCEL = 0.02
 
 # position [m], rotation [rad]
 HOME_TARGET = m3d.Transform((-0.495, 0.145, 0.416, -2.068, -2.365, 0.000))
-ABOVE_ROD_0_TARGET = m3d.Transform((-0.502, 0.345, 0.259, -2.068, -2.365, 0.000))
+ABOVE_ROD_0_TARGET = m3d.Transform((-0.502, 0.345, 0.255, -2.068, -2.365, 0.000))
 
 # length in meters
 ROD_DISTANCE = 0.202  # 20 cm plus error
 DISK_HEIGHT = 0.03
-DISK_DIAMETERS = [0.13, 0.11, 0.9, 0.7]
+DISK_DIAMETERS = [700, 900, 1100, 1300]  # 1/10mm, like onrobot rg likes it
 NUM_DISKS = 4
 
 
-def openGripper() -> None:
-    rg.open_gripper()
-
-    while True:
-        sleep(0.5)
-        if not rg.get_status()[0]:
-            break
-
-
-def closeGripper() -> None:
-    rg.move_gripper(500, 250)
+def moveGripper(width: int, force: int = 250) -> None:
+    rg.move_gripper(width, force)
 
     while True:
         sleep(0.5)
@@ -65,31 +56,32 @@ def hanoi(
 
 
 def executeHanoi() -> None:
-    openGripper()
     rods = [NUM_DISKS, 0, 0]
     for disk, src, dest in hanoi(NUM_DISKS, 0, 1, 2):
-        # go above src rod
-        aboveSrcTarget = translate(ABOVE_ROD_0_TARGET, y=-ROD_DISTANCE * src)
-        rob.set_pose(aboveSrcTarget, vel=FAST_VEL, acc=ACCEL)
-
-        # adjust height of gripper based on where the current disk is on src rod
+        aboveSrcTarget = translate(
+            ABOVE_ROD_0_TARGET, y=-ROD_DISTANCE * src, x=(-0.01 if disk >= 4 else 0)
+        )  # adjust x for larger disk(s)
         pickupSrcTarget = translate(
-            aboveSrcTarget, z=-0.217 + DISK_HEIGHT * (rods[src] - 1)
+            aboveSrcTarget, z=-0.213 + DISK_HEIGHT * (rods[src] - 1)
         )
+        aboveDestTarget = translate(
+            ABOVE_ROD_0_TARGET, y=-ROD_DISTANCE * dest, x=(-0.01 if disk >= 4 else 0)
+        )
+        dropoffDestTarget = translate(
+            aboveDestTarget, z=-0.207 + DISK_HEIGHT * rods[dest]
+        )
+
+        rob.set_pose(aboveSrcTarget, vel=FAST_VEL, acc=ACCEL, wait=False)
+        moveGripper(DISK_DIAMETERS[disk - 1] + 100)  # open extra
         rob.set_pose(pickupSrcTarget, vel=SLOW_VEL, acc=ACCEL)
-        closeGripper()
+        moveGripper(
+            500
+        )  # don't close all the way so in case of an error a rod isn't grabbed
         rob.set_pose(aboveSrcTarget, vel=SLOW_VEL, acc=ACCEL)
 
-        # go above dest rod
-        aboveDestTarget = translate(ABOVE_ROD_0_TARGET, y=-ROD_DISTANCE * dest)
         rob.set_pose(aboveDestTarget, vel=FAST_VEL, acc=ACCEL)
-
-        # adjust height of gripper based on where to put the disk on dest rod
-        dropoffDestTarget = translate(
-            aboveDestTarget, z=-0.210 + DISK_HEIGHT * rods[dest]
-        )
         rob.set_pose(dropoffDestTarget, vel=SLOW_VEL, acc=ACCEL)
-        openGripper()
+        moveGripper(DISK_DIAMETERS[disk - 1] + 100)  # open extra
         rob.set_pose(aboveDestTarget, vel=SLOW_VEL, acc=ACCEL)
 
         rods[src] -= 1
@@ -101,10 +93,8 @@ def main() -> None:
     sleep(0.2)  # give time for robot to process setup commands
 
     rob.set_pose(HOME_TARGET, vel=FAST_VEL, acc=ACCEL, command="movej")
-
     executeHanoi()
-
-    rob.set_pose(HOME_TARGET, vel=SLOW_VEL, acc=ACCEL, command="movej")
+    rob.set_pose(HOME_TARGET, vel=FAST_VEL, acc=ACCEL, command="movej")
 
 
 if __name__ == "__main__":
